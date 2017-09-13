24dbc23aae1fcf8b0d95e5c0d70afe8d3bb43219
JhonC11 <jhon.qortes@gamil.com>
2017-09-13 11:29:50 -0500

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -301,6 +301,21 @@
 	</div>
 </section>
 
+<section class="galeria">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-5">
+				<h1 class="text-center">Galeria CPIC</h1>
+			</div>
+			<div class="col-md-7">
+				<div class="container-gallery">
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
 
 @endsection
 
